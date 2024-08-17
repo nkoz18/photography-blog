c753a5d36970eb9a9448495e0bbb714e4e14a36b
Nikita Kozlov <nikita@stroika.io>
2024-08-17 17:59:04 +0000

    reconfigure for new endpoint

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,21 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "photography-blog-db.ckmckf7lbra5.us-west-2.rds.amazonaws.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "TmiY7bdr22WCB7N"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: true,
     useNullAsDefault: true,
+    acquireConnectionTimeout: 1000000,
   },
 });
