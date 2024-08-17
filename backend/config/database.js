4a29ce2d8f73f0401e785f5d7d4bfd13a2701ab3
nkoz18 <nikita@stroika.io>
2024-08-17 11:55:55 -0700

    Updated incorrect AWS endpoint references

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'photography-blog-db.ckmckf7lbra5.us-west-2.rds.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'TmiY7bdr22WCB7N'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Keep default for schema if needed
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
