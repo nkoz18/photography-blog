71bc23728a97ed2296e320768514c9f27bb27937
nkoz18 <nikita@stroika.io>
2024-08-17 11:39:47 -0700

    Updated incorrect AWS endpoint references

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "photography-blog-db.ckmckf7lbra5.us-west-2.rds.amazonaws.com"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "TmiY7bdr22WCB7N"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      host: env('DATABASE_HOST', 'photography-blog-db.ckmckf7lbra5.us-west-2.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'TmiY7bdr22WCB7N'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
     debug: true,
@@ -22,25 +17,3 @@ module.exports = ({ env }) => ({
     acquireConnectionTimeout: 1000000,
   },
 });
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env(
-//         "DATABASE_HOST",
-//         ""
-//       ),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USERNAME", ""),
-//       password: env("DATABASE_PASSWORD", ""),
-//       schema: env("DATABASE_SCHEMA", "public"), // Not required
-//       ssl: {
-//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-//       },
-//     },
-//     debug: true,
-//     useNullAsDefault: true,
-//     acquireConnectionTimeout: 1000000,
-//   },
-// });
